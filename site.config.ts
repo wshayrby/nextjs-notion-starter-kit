e59eea4e495f3f1413336777561a81bc6a72be63
wshayrby <mermermer@g.kmou.ac.kr>
2026-05-21 10:49:24 +0900

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '317661362ab2816ab875f2c5f69dc488',
+  https://mermermer.oopy.io/: '317661362ab2816ab875f2c5f69dc488',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
